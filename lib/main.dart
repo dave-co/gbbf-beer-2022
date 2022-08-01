e62ac1765911b629667582f72850c7ba2cc36a01
dave <dave@dc-6700k>
2022-08-01 22:50:04 +0100

    search form layout

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   var beers = [];
+  bool showSearch = false;
+  final searchTextController = TextEditingController();
+  String searchText = '';
+
+  bool nameSearch = true;
+  bool notesSearch = false;
+  bool brewerySearch = false;
+  bool barSearch = true;
+  bool styleSearch = false;
+  bool countrySearch = false;
+
+  RangeValues abvRange = const RangeValues(3.8, 12);
+
+  bool onlyShowWants = false;
+  bool onlyShowFavourites = false;
+  bool onlyShowTried = false;
+
+  void _search(){
+    debugPrint("beer - _search on \"${searchTextController.text}\" searching fields: "
+        "${nameSearch == true ? 'name ' : ''}${notesSearch == true ? 'notes ' : ''}${brewerySearch == true ? 'brewery ' : ''}"
+        "${barSearch == true ? 'bar ' : ''}${styleSearch == true ? 'style ' : ''}${countrySearch == true ? 'country ' : ''} within abv range ${abvRange.start} to ${abvRange.end}" );
+
+  }
 
   void _incrementCounter() {
     debugPrint("beer - _incrementCounter");
@@ -78,17 +101,246 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Gbbf Beers 2022'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child : GestureDetector(
+              onTap: (){
+                setState(() {
+                  showSearch = !showSearch;
+                });
+                debugPrint("tap");
+              },
+              child: const Icon(
+                  Icons.search,
+                  size: 26
+              )
+            )
+          )
+        ],
       ),
-      body: ListView.builder(
-          itemBuilder: (BuildContext context, int i) {
-            return Text('${beers[i].name}');
-          }),
+      body: Stack(
+        children: [
+          ListView.builder(
+              itemBuilder: (BuildContext context, int i) {
+                return Column(children: [const Divider(height: 10,),
+                  Row(
+                      children: [
+                        Expanded(
+                            flex: 4,
+                            child: Text('${beers[i].name}')
+                        ),
+                        Expanded(
+                            flex: 2,
+                            child: Text('${beers[i].abv}%')
+                        ),
+                        const Expanded(
+                            flex: 2,
+                            child: Text(' ')
+                        )
+
+                      ]),
+                  Row(
+                      children: [
+                        Expanded(
+                            flex: 4,
+                            child: Text('  ${beers[i].brewery}')
+                        ),
+                        Expanded(
+                            flex: 2,
+                            child: Text('${beers[i].style}')
+                        ),
+                        Expanded(
+                            flex: 2,
+                            child: Text('${beers[i].barCode}')
+                        )
+                      ])
+                ]);
+              }),
+          Visibility(
+              visible: showSearch,
+              child: Container(
+                color: Colors.white,
+                height: 310, // TODO tweak this
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                            child: TextField(
+                              controller: searchTextController,
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Search...'
+                              ),
+                            )
+                        )
+                      ],
+                    ),
+                    Row(
+                      children :[
+                        Expanded(
+                          flex: 2,
+                          child: CheckboxListTile(
+                            title: const Text('Name'),
+                            value: nameSearch,
+                            onChanged: (bool? value){
+                              setState((){
+                                nameSearch = !nameSearch;
+                                _search();
+                              });
+                            },
+                          )
+                        ),
+                        Expanded(
+                            flex: 2,
+                            child: CheckboxListTile(
+                              title: const Text('Notes'),
+                              value: notesSearch,
+                              onChanged: (bool? value){
+                                setState((){
+                                  notesSearch = !notesSearch;
+                                  _search();
+                                });
+                              },
+                            )
+                        ),
+                        Expanded(
+                            flex: 2,
+                            child: CheckboxListTile(
+                              title: const Text('Brewery'),
+                              value: brewerySearch,
+                              contentPadding: const EdgeInsets.all(5),
+                              onChanged: (bool? value){
+                                setState((){
+                                  brewerySearch = !brewerySearch;
+                                  _search();
+                                });
+                              },
+                            )
+                        )
+                      ]
+                    ),
+                    Row(
+                        children :[
+                          Expanded(
+                              flex: 2,
+                              child: CheckboxListTile(
+                                title: const Text('Bar'),
+                                value: barSearch,
+                                onChanged: (bool? value){
+                                  setState((){
+                                    barSearch = !barSearch;
+                                    _search();
+                                  });
+                                },
+                              )
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: CheckboxListTile(
+                                title: const Text('Style'),
+                                value: styleSearch,
+                                onChanged: (bool? value){
+                                  setState((){
+                                    styleSearch = !styleSearch;
+                                    _search();
+                                  });
+                                },
+                              )
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: CheckboxListTile(
+                                title: const Text('Country'),
+                                value: countrySearch,
+                                contentPadding: const EdgeInsets.all(5),
+                                onChanged: (bool? value){
+                                  setState((){
+                                    countrySearch = !countrySearch;
+                                    _search();
+                                  });
+                                },
+                              )
+                          )
+                        ]
+                    ),
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 17),
+                          child: Text('ABV', textScaleFactor: 1.1),
+                        ),
+                        RangeSlider(
+                            values: abvRange,
+                            max: 20, //TODO tweak based on max abv in list
+
+                            onChanged: (RangeValues values) {
+                              setState(() {
+                                abvRange = values;
+                                _search();
+                              });
+                            }
+                        )
+                      ],
+                    ),
+                    Row(
+                        children :[
+                          Expanded(
+                              flex: 2,
+                              child: CheckboxListTile(
+                                title: const Text('Want'),
+                                value: onlyShowWants,
+                                onChanged: (bool? value){
+                                  setState((){
+                                    onlyShowWants = !onlyShowWants;
+                                    _search();
+                                  });
+                                },
+                              )
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: CheckboxListTile(
+                                title: const Text('Tried'),
+                                value: onlyShowTried,
+                                onChanged: (bool? value){
+                                  setState((){
+                                    onlyShowTried = !onlyShowTried;
+                                    _search();
+                                  });
+                                },
+                              )
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: CheckboxListTile(
+                                title: const Text('Favourite'),
+                                value: onlyShowFavourites,
+                                contentPadding: const EdgeInsets.all(5),
+                                onChanged: (bool? value){
+                                  setState((){
+                                    onlyShowFavourites = !onlyShowFavourites;
+                                    _search();
+                                  });
+                                },
+                              )
+                          )
+                        ]
+                    ),
+                  ],
+                )
+              )
+          ),
+        ],
+      )
     );
   }
 
   @override
   void initState() {
       super.initState();
+      searchTextController.addListener(_search);
       loadBeers();
   }
 
